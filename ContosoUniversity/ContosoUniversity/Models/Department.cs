cddc4742440df6f31ba88c523648fcac873790fa
Unknown <unknown@example.com>
2021-02-03 13:16:31 +0800

    處理並行衝突

--- ContosoUniversity/ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/ContosoUniversity/Models/Department.cs
@@ -23,6 +23,13 @@ namespace ContosoUniversity.Models
 
         public int? InstructorID { get; set; }
 
+        // Timestamp 屬性會指定此資料行會包含在傳送到資料庫之 Update 和 Delete 命令的 Where 子句中。
+        // 另一種寫法 DbContext 的 OnModelCreating 中加入
+        // modelBuilder.Entity<Department>().Property(p => p.RowVersion).IsConcurrencyToken();
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
